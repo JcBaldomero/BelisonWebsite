58803af40abd1ce90f72b92261e75573e7be042d
Xcali <82700373+XcaliGit@users.noreply.github.com>
2021-09-19 15:40:58 +0100

    Initial commit

---
+++ Javascript/ImageComparison.js
@@ -0,0 +1,11 @@
+
+
+document.querySelector(".compare #compare-ip").addEventListener("input",function(e){
+    document.querySelector(".compare .before .after").style.width = (+e.target.value) + "px";
+  });
+  
+  document.querySelector(".compare #compare-ip").addEventListener("change",function(e){
+    document.querySelector(".compare .before .after").style.width = (+e.target.value) + "px";
+  });
+  
+  
